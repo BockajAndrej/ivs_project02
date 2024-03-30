f2771ec2011022c8571eae5cb816adbf4ccb17ad
Burr3to <filoj.jf@gmail.com>
2024-03-30 20:03:43 +0100

    Implemented basic operations. Overflow checked added. (+,-,*,/,Factorial)

--- CalculatorApp/Math_lib.cs
+++ CalculatorApp/Math_lib.cs
@@ -12,27 +12,57 @@ namespace CalculatorApp
     }
     public class Math_lib
     {
-        public decimal Count(decimal input1, decimal input2)
+        public decimal Count(decimal v1, decimal v2)
         {
-            throw new NotImplementedException();
+            //checked add time for calcula
+            checked // overflow check, throws overflowexception
+            {
+                return v1 + v2;
+            }
         }
-        public decimal Substraction(decimal input1, decimal input2)
+        public decimal Substraction(decimal v1, decimal v2)
         {
-            throw new NotImplementedException();
+            checked // overflow check, throws overflowexception
+            {
+                return v1 - v2;
+            }
         }
         public decimal Multiplication(decimal v1, decimal v2)
         {
-            throw new NotImplementedException();
+            checked // overflow check, throws overflowexception
+            {
+                return v1 * v2;
+            }
         }
 
         public decimal Division(decimal v1, decimal v2)
         {
-            throw new NotImplementedException();
+            if(v2 != 0)
+            {
+                return v1 / v2;
+            }
+            else throw new DivideByZeroException("Cannot divide by zero");
         }
 
         public decimal Faktorial(decimal v1)
         {
-            throw new NotImplementedException();
+            if (v1 == 0)
+            {
+                return 1;
+            }
+            else if (v1 < 0)
+            {
+                throw new NegativeFactorialException("Factorial is not defined for negative numbers");
+            }
+            else // v1 is positive
+            {
+                decimal result = 1;
+                for (decimal i = 2; i <= v1; i++)
+                {
+                    result *= i;
+                }
+                return result;
+            }
         }
     }
 }
