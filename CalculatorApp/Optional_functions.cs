a5882c4f07a0b0607bbae366a629401956c5193f
Richard Tokušev <xtokusr00@vutbr.cz>
2024-04-15 17:17:19 +0200

    MFile: Optional_functions.cs MFunction: All Brief: Added comments

--- CalculatorApp/Optional_functions.cs
+++ CalculatorApp/Optional_functions.cs
@@ -19,46 +19,111 @@ namespace CalculatorApp
         /// <param name="v3">To</param>
         /// <returns>Decimal value</returns>
         /// <exception cref="NotImplementedException"></exception>
+        /// Degrees, rad
         public decimal Degrees(decimal v1, string v2, string v3)
         {
             throw new NotImplementedException();
         }
-
+        /// <summary>
+        /// A function to calculate density
+        /// </summary>
+        /// <param name="v1"></param>
+        /// <param name="v2"></param>
+        /// <param name="v3"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        /// kg/m3, g/m3, g/cm3, kg/cm3
         public decimal Density(decimal v1, string v2, string v3)
         {
             throw new NotImplementedException();
         }
-
+        /// <summary>
+        /// A function to calculate amount of energy
+        /// </summary>
+        /// <param name="v1"></param>
+        /// <param name="v2"></param>
+        /// <param name="v3"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        /// Joule, kilojoule
         public decimal Energy(decimal v1, string v2, string v3)
         {
             throw new NotImplementedException();
         }
-
+        /// <summary>
+        /// A function to calculate length
+        /// </summary>
+        /// <param name="v1"></param>
+        /// <param name="v2"></param>
+        /// <param name="v3"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        /// mm, cm, dm, m, km, miles
         public decimal Length(decimal v1, string v2, string v3)
         {
             throw new NotImplementedException();
         }
-
+        /// <summary>
+        /// A funtion to calculate performance
+        /// </summary>
+        /// <param name="v1"></param>
+        /// <param name="v2"></param>
+        /// <param name="v3"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        /// Watt, kilowatt
         public decimal Performance(decimal v1, string v2, string v3)
         {
             throw new NotImplementedException();
         }
-
+        /// <summary>
+        /// A function to calculate speed
+        /// </summary>
+        /// <param name="v1"></param>
+        /// <param name="v2"></param>
+        /// <param name="v3"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        /// km/h, miles/h, m/s
         public decimal Speed(decimal v1, string v2, string v3)
         {
             throw new NotImplementedException();
         }
-
+        /// <summary>
+        /// A function to calculate temperature
+        /// </summary>
+        /// <param name="v1"></param>
+        /// <param name="v2"></param>
+        /// <param name="v3"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        /// Celsius, fahrenheit
         public decimal Temp(decimal v1, string v2, string v3)
         {
             throw new NotImplementedException();
         }
-
+        /// <summary>
+        /// A funtion to calculate time units
+        /// </summary>
+        /// <param name="v1"></param>
+        /// <param name="v2"></param>
+        /// <param name="v3"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        /// sec, min, hour, days
         public decimal Time(decimal v1, string v2, string v3)
         {
             throw new NotImplementedException();
         }
-
+        /// <summary>
+        /// A function to calculate weight
+        /// </summary>
+        /// <param name="v1"></param>
+        /// <param name="v2"></param>
+        /// <param name="v3"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        /// mg, g, kg, ton
         public decimal Weight(decimal v1, string v2, string v3)
         {
             throw new NotImplementedException();
