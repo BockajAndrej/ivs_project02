2ee400a978bcb1448beafc0fcbc64c3d2cf9d279
Andrej Bočkaj <106064818+BockajAndrej@users.noreply.github.com>
2024-04-15 19:45:16 +0200

    MFile: UnitTest2.cs MFunction: Each function from Test_weight() to Test_degrees() Brief: Added implementation. Tests should be done.

--- CalculatorApp/Optional_functions.cs
+++ CalculatorApp/Optional_functions.cs
@@ -8,6 +8,15 @@ namespace CalculatorApp
 {
     public class Optional_functions
     {
+        public class UndefinedUnitException : Exception
+        {
+            public UndefinedUnitException(string message) : base(message) { }
+        }
+        public class NegativeNumberException : Exception
+        {
+            public NegativeNumberException(string message) : base(message) { }
+        }
+
         // Create an instance to Math functions:
         Math_lib root = new Math_lib();
 
@@ -19,7 +28,7 @@ namespace CalculatorApp
         /// <param name="v3">To</param>
         /// <returns>Decimal value</returns>
         /// <exception cref="NotImplementedException"></exception>
-        /// Degrees, rad
+        /// degrees(deg), rad
         public decimal Degrees(decimal v1, string v2, string v3)
         {
             throw new NotImplementedException();
@@ -27,9 +36,9 @@ namespace CalculatorApp
         /// <summary>
         /// A function to calculate density
         /// </summary>
-        /// <param name="v1"></param>
-        /// <param name="v2"></param>
-        /// <param name="v3"></param>
+        /// <param name="v1">Value</param>
+        /// <param name="v2">From</param>
+        /// <param name="v3">To</param>
         /// <returns></returns>
         /// <exception cref="NotImplementedException"></exception>
         /// kg/m3, g/m3, g/cm3, kg/cm3
@@ -40,12 +49,12 @@ namespace CalculatorApp
         /// <summary>
         /// A function to calculate amount of energy
         /// </summary>
-        /// <param name="v1"></param>
-        /// <param name="v2"></param>
-        /// <param name="v3"></param>
+        /// <param name="v1">Value</param>
+        /// <param name="v2">From</param>
+        /// <param name="v3">To</param>
         /// <returns></returns>
         /// <exception cref="NotImplementedException"></exception>
-        /// Joule, kilojoule
+        /// joule(J), kilojoule(kJ)
         public decimal Energy(decimal v1, string v2, string v3)
         {
             throw new NotImplementedException();
@@ -53,12 +62,12 @@ namespace CalculatorApp
         /// <summary>
         /// A function to calculate length
         /// </summary>
-        /// <param name="v1"></param>
-        /// <param name="v2"></param>
-        /// <param name="v3"></param>
+        /// <param name="v1">Value</param>
+        /// <param name="v2">From</param>
+        /// <param name="v3">To</param>
         /// <returns></returns>
         /// <exception cref="NotImplementedException"></exception>
-        /// mm, cm, dm, m, km, miles
+        /// mm, cm, dm, m, km, miles, inch, yard
         public decimal Length(decimal v1, string v2, string v3)
         {
             throw new NotImplementedException();
@@ -66,12 +75,12 @@ namespace CalculatorApp
         /// <summary>
         /// A funtion to calculate performance
         /// </summary>
-        /// <param name="v1"></param>
-        /// <param name="v2"></param>
-        /// <param name="v3"></param>
+        /// <param name="v1">Value</param>
+        /// <param name="v2">From</param>
+        /// <param name="v3">To</param>
         /// <returns></returns>
         /// <exception cref="NotImplementedException"></exception>
-        /// Watt, kilowatt
+        /// watt, kilowatt
         public decimal Performance(decimal v1, string v2, string v3)
         {
             throw new NotImplementedException();
@@ -79,9 +88,9 @@ namespace CalculatorApp
         /// <summary>
         /// A function to calculate speed
         /// </summary>
-        /// <param name="v1"></param>
-        /// <param name="v2"></param>
-        /// <param name="v3"></param>
+        /// <param name="v1">Value</param>
+        /// <param name="v2">From</param>
+        /// <param name="v3">To</param>
         /// <returns></returns>
         /// <exception cref="NotImplementedException"></exception>
         /// km/h, miles/h, m/s
@@ -92,12 +101,12 @@ namespace CalculatorApp
         /// <summary>
         /// A function to calculate temperature
         /// </summary>
-        /// <param name="v1"></param>
-        /// <param name="v2"></param>
-        /// <param name="v3"></param>
+        /// <param name="v1">Value</param>
+        /// <param name="v2">From</param>
+        /// <param name="v3">To</param>
         /// <returns></returns>
         /// <exception cref="NotImplementedException"></exception>
-        /// Celsius, fahrenheit
+        /// celsius(C), fahrenheit(F), kelvin(K)
         public decimal Temp(decimal v1, string v2, string v3)
         {
             throw new NotImplementedException();
@@ -105,12 +114,12 @@ namespace CalculatorApp
         /// <summary>
         /// A funtion to calculate time units
         /// </summary>
-        /// <param name="v1"></param>
-        /// <param name="v2"></param>
-        /// <param name="v3"></param>
+        /// <param name="v1">Value</param>
+        /// <param name="v2">From</param>
+        /// <param name="v3">To</param>
         /// <returns></returns>
         /// <exception cref="NotImplementedException"></exception>
-        /// sec, min, hour, days
+        /// sec, min, hour, days, weeks, months, years
         public decimal Time(decimal v1, string v2, string v3)
         {
             throw new NotImplementedException();
@@ -118,12 +127,12 @@ namespace CalculatorApp
         /// <summary>
         /// A function to calculate weight
         /// </summary>
-        /// <param name="v1"></param>
-        /// <param name="v2"></param>
-        /// <param name="v3"></param>
+        /// <param name="v1">Value</param>
+        /// <param name="v2">From</param>
+        /// <param name="v3">To</param>
         /// <returns></returns>
         /// <exception cref="NotImplementedException"></exception>
-        /// mg, g, kg, ton
+        /// mg, dag, g, kg, t, lb, oz
         public decimal Weight(decimal v1, string v2, string v3)
         {
             throw new NotImplementedException();
--- UnitTestCalculator/UnitTest2.cs
+++ UnitTestCalculator/UnitTest2.cs
@@ -1,6 +1,7 @@
 ﻿using CalculatorApp;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using static CalculatorApp.Optional_functions;
 
 namespace UnitTestCalculator
 {
@@ -15,38 +16,125 @@ namespace UnitTestCalculator
         /// <summary>
         /// Testing Weight() function for weight conversions 
         /// </summary>
+        /// mg, dag, g, kg, t, lb, oz
         [TestMethod]
         public void Test_weight()
         {
             Assert.ThrowsException<OverflowException>(() => root.Weight(79228162514264337593543950334m, "kg", "g"));
-            Assert.AreEqual(12m, root.Weight(12m, "kg", "kg"), delta: 0);
+            Assert.ThrowsException<UndefinedUnitException>(() => root.Weight(1m, "kk", "g"));
+            Assert.ThrowsException<UndefinedUnitException>(() => root.Weight(1m, "kg", "gg"));
+
+            /// From kg
+            Assert.AreEqual(123.4567m, root.Weight(123.4567m, "kg", "kg"), delta: 0);
+            Assert.AreEqual(123456700m, root.Weight(123.4567m, "kg", "mg"), delta: 0);
+            Assert.AreEqual(123456.7m, root.Weight(123.4567m, "kg", "g"), delta: 0);
+            Assert.AreEqual(272.1756557m, root.Weight(123.4567m, "kg", "lb"), delta: 0);
+            Assert.AreEqual(0.1234567m, root.Weight(123.4567m, "kg", "t"), delta: 0);
+            Assert.AreEqual(12345.67m, root.Weight(123.4567m, "kg", "dag"), delta: 0);
+            Assert.AreEqual(4354.81m, root.Weight(123.4567m, "kg", "oz"), delta: 0);
+            /// To mg
+            Assert.AreEqual(4658.1345m, root.Weight(4658.1345m, "mg", "mg"), delta: 0);
+            Assert.AreEqual(4658134500m, root.Weight(4658.1345m, "kg", "mg"), delta: 0);
+            Assert.AreEqual(4658134.5m, root.Weight(4658.1345m, "g", "mg"), delta: 0);
+            Assert.AreEqual(2112894268m, root.Weight(4658.1345m, "lb", "mg"), delta: 0);
+            Assert.AreEqual(4658134500000m, root.Weight(4658.1345m, "t", "mg"), delta: 0);
+            Assert.AreEqual(46581345m, root.Weight(4658.1345m, "dag", "mg"), delta: 0);
+            Assert.AreEqual(132055784.01m, root.Weight(4658.1345m, "oz", "mg"), delta: 0);
+            /// Negative
+            Assert.AreEqual(-0.0682587m, root.Weight(-68.25867m, "g", "kg"), delta: 0);
+            Assert.AreEqual(-30961.5866426m, root.Weight(-68.25867m, "lb", "g"), delta: 0);
+            /// Zero
+            Assert.AreEqual(0m, root.Weight(0m, "g", "kg"), delta: 0);
+
         }
         /// <summary>
         /// Testing Length() function for length conversions
         /// </summary>
+        /// mm, cm, dm, m, km, miles, inch, yard
         [TestMethod]
         public void Test_length() 
         {
             Assert.ThrowsException<OverflowException>(() => root.Length(79228162514264337593543950334m, "km", "m"));
-            Assert.AreEqual(12m, root.Length(12m, "kg", "kg"), delta: 0);
+            Assert.ThrowsException<NegativeNumberException>(() => root.Length(-5m, "km", "m"));
+            Assert.ThrowsException<UndefinedUnitException>(() => root.Length(1m, "kk", "g"));
+            Assert.ThrowsException<UndefinedUnitException>(() => root.Length(1m, "km", "ms"));
+
+            /// From dm
+            Assert.AreEqual(1234.567m, root.Weight(123.4567m, "cm", "mm"), delta: 0);
+            Assert.AreEqual(123.4567m, root.Weight(123.4567m, "cm", "cm"), delta: 0);
+            Assert.AreEqual(12.34567m, root.Weight(123.4567m, "cm", "dm"), delta: 0);
+            Assert.AreEqual(1.234567m, root.Weight(123.4567m, "cm", "m"), delta: 0);
+            Assert.AreEqual(0.001235m, root.Weight(123.4567m, "cm", "km"), delta: 0);
+            Assert.AreEqual(0.000767m, root.Weight(123.4567m, "cm", "miles"), delta: 0);
+            Assert.AreEqual(48.605m, root.Weight(123.4567m, "cm", "inch"), delta: 0);
+            Assert.AreEqual(1.350139m, root.Weight(123.4567m, "cm", "yard"), delta: 0);
+            /// To km
+            Assert.AreEqual(123.4567m, root.Weight(98756.2586m, "mm", "km"), delta: 0);
+            Assert.AreEqual(0.09875626m, root.Weight(98756.2586m, "cm", "km"), delta: 0);
+            Assert.AreEqual(0.98756259m, root.Weight(98756.2586m, "dm", "km"), delta: 0);
+            Assert.AreEqual(98.7562586m, root.Weight(98756.2586m, "m", "km"), delta: 0);
+            Assert.AreEqual(98756.2586m, root.Weight(98756.2586m, "km", "km"), delta: 0);
+            Assert.AreEqual(158932.792m, root.Weight(98756.2586m, "miles", "km"), delta: 0);
+            Assert.AreEqual(2.50840897m, root.Weight(98756.2586m, "inch", "km"), delta: 0);
+            Assert.AreEqual(90.3027229m, root.Weight(98756.2586m, "yard", "km"), delta: 0);
+            /// Zero
+            Assert.AreEqual(0m, root.Weight(0m, "mm", "km"), delta: 0);
         }
         /// <summary>
         /// Testing Time() function for time conversions
         /// </summary>
+        /// sec, min, hour, days, weeks, months, years
         [TestMethod]
         public void Test_time() 
         {
             Assert.ThrowsException<OverflowException>(() => root.Time(79228162514264337593543950334m, "min", "sec"));
-            Assert.AreEqual(12m, root.Time(12m, "kg", "kg"), delta: 0);
+            Assert.ThrowsException<NegativeNumberException>(() => root.Time(-5m, "min", "sec"));
+            Assert.ThrowsException<UndefinedUnitException>(() => root.Time(1m, "miy", "sec"));
+            Assert.ThrowsException<UndefinedUnitException>(() => root.Time(1m, "day", "yeer"));
+
+            /// From hour
+            Assert.AreEqual(444444.1m, root.Time(123.4567m, "hour", "sec"), delta: 0);
+            Assert.AreEqual(7407.402m, root.Time(123.4567m, "hour", "min"), delta: 0);
+            Assert.AreEqual(1234.567m, root.Time(123.4567m, "hour", "hour"), delta: 0);
+            Assert.AreEqual(5.144029m, root.Time(123.4567m, "hour", "days"), delta: 0);
+            Assert.AreEqual(0.734861m, root.Time(123.4567m, "hour", "weeks"), delta: 0);
+            Assert.AreEqual(0.169003m, root.Time(123.4567m, "hour", "months"), delta: 0);
+            Assert.AreEqual(0.014084m, root.Time(123.4567m, "hour", "years"), delta: 0);
+            /// To days
+            Assert.AreEqual(1.14301225m, root.Time(98756.2586m, "sec", "days"), delta: 0);
+            Assert.AreEqual(68.5807351m, root.Time(98756.2586m, "min", "days"), delta: 0);
+            Assert.AreEqual(4114.84411m, root.Time(98756.2586m, "hour", "days"), delta: 0);
+            Assert.AreEqual(98756.2586m, root.Time(98756.2586m, "days", "days"), delta: 0);
+            Assert.AreEqual(691293.81m, root.Time(98756.2586m, "weeks", "days"), delta: 0);
+            Assert.AreEqual(135.1899502m, root.Time(98756.2586m, "months", "days"), delta: 0);
+            Assert.AreEqual(36070723.5m, root.Time(98756.2586m, "year", "days"), delta: 0);
+            ///Zero 
+            Assert.AreEqual(0m, root.Time(0m, "hour", "day"), delta: 0);
         }
         /// <summary>
         /// Testing Temp() function for temperature conversions
         /// </summary>
+        /// celsius(C), fahrenheit(F), kelvin(K)
         [TestMethod]
         public void Test_temp() 
         {
             Assert.ThrowsException<OverflowException>(() => root.Temp(79228162514264337593543950334m, "C", "K"));
-            Assert.AreEqual(12m, root.Temp(12m, "kg", "kg"), delta: 0);
+            Assert.ThrowsException<UndefinedUnitException>(() => root.Temp(1m, "L", "K"));
+            Assert.ThrowsException<UndefinedUnitException>(() => root.Temp(1m, "C", "S"));            
+
+            /// From C
+            Assert.AreEqual(123.4567m, root.Temp(123.4567m, "C", "C"), delta: 0);
+            Assert.AreEqual(254.2221m, root.Temp(123.4567m, "C", "F"), delta: 0);
+            Assert.AreEqual(396.6067m, root.Temp(123.4567m, "C", "K"), delta: 0);
+            /// To K
+            Assert.AreEqual(4931.2845m, root.Temp(4658.1345m, "C", "K"), delta: 0);
+            Assert.AreEqual(2843.2247m, root.Temp(4658.1345m, "F", "K"), delta: 0);
+            Assert.AreEqual(4658.1345m, root.Temp(4658.1345m, "K", "K"), delta: 0);
+            /// Negative
+            Assert.AreEqual(-90.86561m, root.Temp(-68.25867m, "C", "F"), delta: 0);
+            Assert.AreEqual(217.4507m, root.Temp(-68.25867m, "F", "K"), delta: 0);
+            /// Zero
+            Assert.AreEqual(32m, root.Temp(0m, "C", "F"), delta: 0);
         }
         /// <summary>
         /// Testing Degrees() function for plane angle conversions
@@ -54,8 +142,21 @@ namespace UnitTestCalculator
         [TestMethod]
         public void Test_degrees() 
         {
-            Assert.ThrowsException<OverflowException>(() => root.Degrees(79228162514264337593543950334m, "rad", "C"));
-            Assert.AreEqual(12m, root.Degrees(12m, "kg", "kg"), delta: 0);
+            Assert.ThrowsException<OverflowException>(() => root.Degrees(79228162514264337593543950334m, "rad", "deg"));
+            Assert.ThrowsException<UndefinedUnitException>(() => root.Temp(1m, "rsd", "deg"));
+            Assert.ThrowsException<UndefinedUnitException>(() => root.Temp(1m, "del", "rad"));
+
+            /// From deg
+            Assert.AreEqual(123.4567m, root.Temp(123.4567m, "deg", "deg"), delta: 0);
+            Assert.AreEqual(2.154726m, root.Temp(123.4567m, "deg", "rad"), delta: 0);
+            /// To rad
+            Assert.AreEqual(4658.1345m, root.Temp(4658.1345m, "rad", "rad"), delta: 0);
+            Assert.AreEqual(266891.45m, root.Temp(4658.1345m, "rad", "deg"), delta: 0);
+            /// Negative
+            Assert.AreEqual(-1.191339m, root.Temp(-68.25867m, "deg", "rad"), delta: 0);
+            Assert.AreEqual(-3910.934m, root.Temp(-68.25867m, "rad", "deg"), delta: 0);
+            /// Zero
+            Assert.AreEqual(0m, root.Temp(0m, "deg", "rad"), delta: 0);
         }
         /// <summary>
         /// Testing Density() function for density conversions
